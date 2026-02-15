20ad3a5c30d67e58284b62903483ebdfa06a459d
Divine Nwose <nwosedivine40@gmail.com>
2026-02-15 14:01:36 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.jsx";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
